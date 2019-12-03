c1cc7b707b150d4ec22e539b5f98a574b9fc6248
bjh <8566300@qq.com>
2019-12-03 14:03:33 +0800

    coordinate canvas.

--- main.js
+++ main.js
@@ -7,6 +7,10 @@
 //canvas 例子
 //https://blog.csdn.net/weixin_43960657/article/details/89309983
 
+//使用VsCode, JsDoc, Js Refactor, Darcula Theme
+
+//js还是太蹩脚了，没有类型系统，要虚拟一个，没有变量是否定义的检查，没有private
+
 /**@type {HTMLCanvasElement} */        
 let canvas ;
 /**@type {CanvasRenderingContext2D} */ 
@@ -19,76 +23,85 @@ class Point{
 }
 
 class Coord{
-    /**@type {number} */  
-    marginInPixel = 5;
+    /**@type {number} */  marginInPixel = 5;
+    /**@type {number} */  pxPerUnit = 5;
     /**@type {number} */  minX = -5;
-    /**@type {number} */  maxX = 20;
     /**@type {number} */  minY = -5 ;
-    /**@type {number} */  maxY = 20 ;
-    /**
-     * 一个单位的像素数
-     * @type {number} 
-     */ 
-     pixelPerUnit = 20 ;
-     /**绘制坐标系*/
+
+    /**@type {number} */  _baseXinPx;
+    /**@type {number} */  _baseYinPx;
+    /**@type {number} */  _widthPx;
+    /**@type {number} */  _heightPx;
+    
+    /**绘制坐标系*/
      paint(){
-         let widthPx = canvas.width;
-         let heightPx = canvas.height;
-         console.log("canvas width="+widthPx+" canvas Height="+heightPx);
-         let totalXUnits = this.maxX - this.minX;
-         let totalYUnits = this.maxY - this.minY;
+         this._widthPx = canvas.width;
+         this._heightPx = canvas.height;
+         console.log("canvas width="+this._widthPx+" canvas Height="+this._heightPx);
+         let totalXUnits = (this._widthPx - 2*this.marginInPixel)/this.pxPerUnit;
+         let totalYUnits = (this._heightPx - 2*this.marginInPixel)/this.pxPerUnit;
          console.log("totalY="+totalYUnits);
          //ctx.beginPath()
          //计算x轴纵坐标
-         let xAxisY = (heightPx - 2*this.marginInPixel)/totalYUnits*this.maxY;
-         let yAxisX = (widthPx  - 2*this.marginInPixel)/totalXUnits*Math.abs(this.minX);
-         console.log("x pos="+xAxisY);
-         ctx.beginPath();
-         //ctx.strokeStyle = "red";
-         //ctx.fillStyle = "black";
-         //绘制x轴
-         ctx.lineWidth = 1;
-         ctx.moveTo(this.marginInPixel+0.5,xAxisY+0.5);
-         ctx.lineTo(widthPx - this.marginInPixel+0.5,xAxisY+0.5);
-         ctx.stroke();
-         ctx.closePath();
-
-         //绘制y轴
-         ctx.moveTo(this.marginInPixel+yAxisX+0.5,this.marginInPixel+0.5);
-         ctx.lineTo(this.marginInPixel+yAxisX+0.5,heightPx - this.marginInPixel+0.5);
-         ctx.stroke();
-         ctx.closePath();
-
-         //绘制刻度
-
-         //绘制网格
-
+         this._baseXinPx = this.marginInPixel + (totalXUnits - Math.abs(this.minX))* this.pxPerUnit; //最大是maxY个刻度，每个刻度是多少
+         this._baseYinPx = this.pxPerUnit * Math.abs(this.minX); //从minX数多少个刻度
+        //  console.log("x pos="+this.baseXinPx);
+        for(let x = this.minX; x <= totalXUnits; x++){
+            this.drawLine(ctx,x,null);
+         }
+         for(let y = this.minY; y<= totalYUnits; y++){
+            this.drawLine(ctx,null,y);
+         }
          //ctx.strokeRect(100.5,100.5,200,300);
          //ctx.fillRect(10,10,60,60);
          //计算y轴横坐标   
-
-
-// 　　1、绘制线条时，采用线条中点定位的方式，所以造成模糊，刚才引用的博文也说得很清楚，只需要坐标偏移到0.5，那么1px宽度的线条就不会出现模糊，
-// 　　比如画一个矩形边框，那么正确的做法就是x和y各偏移0.5，高和宽各缩小1即可，比如，我们要绘制一个20*30的矩形，就应该使用ctx.strokeRect(0.5,0.5,20,30)
-// 　　2、填充矩形，这个和绘制线条又不一样了，给定的坐标就真的是填充的边界，比如刚才举的例子，ctx.fillRect(0.5,0.5,20,30)，如果这样调用，肯定是会模糊的
-// 　　所以这个时候就应该改成ctx.fillRect(0,0,20,30)
      }
-}
-let coord = new Coord();
 
-//用数学坐标绘制一条线。如果仅仅指定x,y为任意值，那就是绘制横线
-/**
- * @param {CanvasRenderingContext2D} ctx 
- * @param {number | null} x 
- * @param {number | null} y 
- */
-function drawLine(ctx,x,y){
-    //要把x转化为屏幕坐标的x， 
-    if(y == null)
-    {
+     //用数学坐标绘制一条线。如果仅仅指定x,y为任意值，那就是绘制横线
+    /**
+     * @param {CanvasRenderingContext2D} ctx 
+     * @param {number | null} x 
+     * @param {number | null} y 
+     */
+    drawLine(ctx,x,y){
+        // 　　1、绘制线条时，采用线条中点定位的方式，所以造成模糊，刚才引用的博文也说得很清楚，只需要坐标偏移到0.5，那么1px宽度的线条就不会出现模糊，
+        // 　　比如画一个矩形边框，那么正确的做法就是x和y各偏移0.5，高和宽各缩小1即可，比如，我们要绘制一个20*30的矩形，就应该使用ctx.strokeRect(0.5,0.5,20,30)
+        // 　　2、填充矩形，这个和绘制线条又不一样了，给定的坐标就真的是填充的边界，比如刚才举的例子，ctx.fillRect(0.5,0.5,20,30)，如果这样调用，肯定是会模糊的
+        // 　　所以这个时候就应该改成ctx.fillRect(0,0,20,30)
+        ctx.strokeStyle = (x === 0 || y === 0)?"black":"lightgray"; 
+        ctx.lineWidth   = (x === 0 || y === 0)? 1:0.5;
+        /**@type {number} */
+        let startX, startY, endX, endY;
+        if(y == null)
+        {
+            startY = endY = this._baseXinPx-x*this.pxPerUnit+0.5;
+            startX = this.marginInPixel + 0.5;
+            endX = this._widthPx - this.marginInPixel+0.5
+            //纵坐标不变，横贯画布
+        }else if(x == null){
+            //纵贯画布
+            startX = endX = this.marginInPixel + this._baseYinPx + y * this.pxPerUnit + 0.5;
+            startY = this.marginInPixel+0.5;
+            endY =this._heightPx - this.marginInPixel+0.5;
+        }
+        ctx.beginPath();
+        ctx.moveTo(startX,startY);
+        ctx.lineTo( endX,endY);
+        ctx.stroke();
+        ctx.closePath();
+    }
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    drawVector(x,y){
 
     }
 }
+let coord = new Coord();
+
+
 function main(){
     console.log("document is ready");
     canvas  = document.getElementById("canvas");
